9d68473f6413c7f11e0a00e8ee605fd1d2a7afc6
Xiaoxian1112 <114861169+Xiaoxian1112@users.noreply.github.com>
2022-11-17 19:05:35 +0000

    Update proj4.R

--- proj4.R
+++ proj4.R
@@ -2,60 +2,63 @@
 newt <- function(theta,func,grad,hess=NULL,...,
                  tol=1e-8, fscale=1,maxit=100,max.half=20,eps=1e-6){
   
-  f0 <- func(theta,...)
-  gradient <- grad(theta,...)
-  if(is.null(hess)){
+  f0 <- func(theta,...) # the initial value of our objective function
+  gradient <- grad(theta,...) # the initial gradient of intial objective function
+  
+  
+  if(is.null(hess)){ # if hessian matrix is not provided,we need to generate an approximation
     hess <- function(theta) {
-      eps <- 1e-8
+      eps <- 1e-8 # finite difference intervals
       gradient <- grad(theta)
-     
-      fd.f <- matrix(0L, nrow = length(theta), ncol = length(theta))
-      for (i in 1:length(theta)){
+      fd.f <- matrix(0L, nrow = length(theta), ncol = length(theta)) # initialize hessian matrix
+      for (i in 1:length(theta)){ 
         th1 <- theta; th1[i] <- th1[i]+eps
         f.hi <- grad(th1)
-        fd.f[,i] <- (f.hi - gradient)/eps
+        fd.f[,i] <- (f.hi - gradient)/eps # generate an approximate hessian matrix by finite differencing of the gradient vector 
       }
       
-      fd.f <- 0.5 * (t(fd.f) + fd.f)
+      fd.f <- 0.5 * (t(fd.f) + fd.f) # get symmetric approximate hessian matrix
       return(fd.f)
     }
     H <- hess(theta)
 
-  }else{
+  }else{ # if hessian matrix is provided, get the initial hessian matrix of intial objective function
   H <- hess(theta,...)
-  H <- 0.5 * (t(H) + H)
+  H <- 0.5 * (t(H) + H) # make hessian matrix symmetric
   }
-
+  
+  #  If the objective or its derivatives are not finite at the initial theta, stop
   if (is.finite(f0)==FALSE | any(is.finite(gradient)==FALSE)| any(is.finite(H)==FALSE)) {
     stop("The objective or its derivatives are not finite at
          your initial estimate of theta.")
   }
-
-  iter = 0
+  
+  # optimization begins
+  iter = 0 # initialize iteration
   while (iter < maxit) {
-    if (max(abs(gradient)) < (abs(f0)+fscale)*tol){# Checking convergence
+    if (max(abs(gradient)) < (abs(f0)+fscale)*tol){# if converged
       cat("Converged",'\n')
-      if (inherits(try(chol(H), silent = TRUE),"try-error")){ 
-       
+      # check if hessian matrix is positive definte at convergence,we cannot get cholesky decomposition if hessian is not positive definite
+      if (inherits(try(chol(H), silent = TRUE),"try-error")){ # if try error, hessian matrix is not positive definite, give warning
         warning("The Hessian is not positive definite at convergence")
       }
      
-      Hi <- ginv(H)
+      Hi <- ginv(H) # inverse hessian matrix
+      # return a list contain the minimum f,the value of theta, iteration, the gradient and inverse of the Hessian matrix at the minimum
       return(list(f.min=f0[1], theta=theta, iter=iter, g=gradient, Hi=Hi))
-    } else {
+    } else { # if not converged
       catch.iter <- 0
-     
-      while(inherits(try(H.chol <- chol(H), silent = TRUE), "try-error") == TRUE) {
-        
+      while(inherits(try(H.chol <- chol(H), silent = TRUE), "try-error") == TRUE) { 
+       #if try error, hessian matrix is not positive definite, we should make it positive definite
         H <- H + diag(abs(max(H))*10^catch.iter*tol, nrow=nrow(H), ncol=ncol(H))
         catch.iter <- catch.iter + 1
       }
-     
+      # newton step, we need to calculate forward step to optimize our function
       Delta <- backsolve(H.chol, forwardsolve(t(H.chol), -gradient))
-      
      
-      half.iter = 0
-      
+      # judge if the step fails to reduce objective function after trying max.half step halving
+      half.iter = 0 # initialize interation
+     
       while ((func(theta + Delta)[1] > f0[1]) |
              is.infinite(func(theta + Delta)) |
              any(is.infinite(grad(theta + Delta))) |

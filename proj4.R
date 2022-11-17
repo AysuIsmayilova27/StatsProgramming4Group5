45e5b2cdd492c40866b6f67d3ebb5c97a813d5f3
Evelyn <113941619+EvelynYan123@users.noreply.github.com>
2022-11-17 18:46:10 +0000

    Update proj4.R

--- proj4.R
+++ proj4.R
@@ -1,11 +1,4 @@
 
-
-
-
-
-
-library('MASS')
-
 newt <- function(theta,func,grad,hess=NULL,...,
                  tol=1e-8, fscale=1,maxit=100,max.half=20,eps=1e-6){
   
@@ -105,24 +98,3 @@ newt <- function(theta,func,grad,hess=NULL,...,
   
   
 }
-th0 <- c(0.5,2)
-
-rb <- function(th,k=2) {# func = objective function
-  # D = k*(x_2 - x_1^2)^2 + (1-x_1)^2
-  k*(th[2]-th[1]^2)^2 + (1-th[1])^2 
-}
-
-gb <- function(th,k=2) {# grad = gardient
-  c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
-}
-
-
-hb <- function(th,k=2) {# hess = Hessian matrix
-  h <- matrix(0,2,2)
-  h[1,1] <- 2-k*2*(2*(th[2]-th[1]^2) - 4*th[1]^2)
-  h[2,2] <- 2*k
-  h[1,2] <- h[2,1] <- -4*k*th[1]
-  h
-}
-
-newt(th0,rb,gb,hb) ## full version
